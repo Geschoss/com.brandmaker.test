361f7fce17936bc6c524b2276396437ce64fbb56
Geschoss <q11254@gmail.com>
2016-04-03 13:50:44 +0600

    step 2. add UserEntity + JPA

---
+++ src/main/java/ru/brandmaker/testUsers/dao/UsersEntity.java
@@ -0,0 +1,87 @@
+package ru.brandmaker.testUsers.dao;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "users", schema = "usersdb")
+public class UsersEntity {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    @Basic
+    @Column(name = "first_name", nullable = false, length = 50)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+
+    @Basic
+    @Column(name = "last_name", nullable = false, length = 50)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "year_of_birth", nullable = false)
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsersEntity that = (UsersEntity) o;
+
+        if (id != that.id) return false;
+        if (yearOfBirth != that.yearOfBirth) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + yearOfBirth;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return this.getId() + ": " +this.getFirstName() + " " + this.getLastName() + "; " + this.getYearOfBirth();
+    }
+}
