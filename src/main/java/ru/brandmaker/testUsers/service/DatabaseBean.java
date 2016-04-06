4e68e5483a673af1aad874aa07fcaa810b44f2c8
Geschoss <q11254@gmail.com>
2016-04-06 15:18:26 +0600

    step 3. JPA+ EJB3+REST

--- src/main/java/ru/brandmaker/testUsers/service/DatabaseBean.java
+++ src/main/java/ru/brandmaker/testUsers/service/DatabaseBean.java
@@ -6,10 +6,19 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
 import java.util.List;
 
+@Path("service")
 @Stateless
-public class UserBean {
+public class DatabaseBean {
+
     @PersistenceContext(unitName = "DEVMODE")
     private EntityManager em;
 
@@ -18,22 +27,28 @@ public class UserBean {
         return em.merge(user);
     }
 
-
-    public UsersEntity get(int id){
-        return em.find(UsersEntity.class, id);
-    }
-
-
     public void update(UsersEntity user){
         add(user);
     }
 
 
-    public void delete(int id){
+    @GET
+    @Path("/getUser/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public UsersEntity get(@PathParam("id") int id){
+        return em.find(UsersEntity.class, id);
+    }
+
+    @GET
+    @Path("/deleteUser/{id}")
+    public void delete(@PathParam("id") int id){
         em.remove(get(id));
     }
 
 
+    @GET
+    @Path("/getAllUsers")
+    @Produces(MediaType.APPLICATION_JSON)
     public List<UsersEntity> getAll(){
         TypedQuery<UsersEntity> namedQuery = em.createNamedQuery("UsersEntity.getAll", UsersEntity.class);
         return namedQuery.getResultList();
