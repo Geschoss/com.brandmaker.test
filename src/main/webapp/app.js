663eb1725da38368cf9685a47ad4a621a86264d2
Geschoss <q11254@gmail.com>
2016-04-11 20:07:05 +0600

    step 3. JPA+ EJB3+REST

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,68 +1,25 @@
-Ext.define('UsersEntity', {
-    extend: 'Ext.data.Model',
+Ext.application({
+    name: 'UserCatalog',
 
-    fields: [{
-        name: 'id',
-        type: 'int'
-    }, {
-        name: 'firstName',
-        type: 'string'
-    }, {
-        name: 'lastName',
-        type: 'string'
-    }, {
-        name: 'yearOfBirth',
-        type: 'int'
-    }]
-});
+    views: [
+        'AddUserFormView',
+        'UserCatalogView',
+        'UserGridView'
+    ],
+    controllers : [
+        'UserController'
+    ],
 
+    stores : [
+        'UserStore'
+    ],
 
-var RESTmembers = Ext.create('Ext.data.Store', {
-    autoLoad: true,
-    autoSync: true,
-    model: 'UsersEntity',
-    proxy: {
-        type: 'rest',
-        url: '/service',
-        reader: {
-            type: 'json',
-            root: 'users',
-            totalProperty: 'success'
-        }
-    }
-});
-
-
-Ext.onReady(function () {
-    var grid =  Ext.create('Ext.grid.Panel', {
-        renderTo: document.body,
-        store: RESTmembers,
-        width: 800,
-        height: 400,
-        title: 'Users',
-        selType: 'rowmodel',
-        columns: [
-            {
-                text: 'id',
-                width: 30,
-                dataIndex: 'id'
-            },
-            {
-                text: 'firstName',
-                width: 150,
-                dataIndex: 'firstName',
-            },
-            {
-                text: 'lastName',
-                flex: 1,
-                dataIndex: 'lastName'
-            },
-            {
-                text: 'yearOfBirth',
-                flex: 1,
-                dataIndex: 'yearOfBirth'
+    launch: function () {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: {
+                xtype  : 'userCatalogView'
             }
-        ]
-    });
-
+        });
+    }
 });
